47cf8783216a012158c70e7e5df0353119cba2cd
gaskeo <zeytunyan2004@mail.ru>
2023-04-24 10:27:25 +0500

    feat(charts): improve bar chart x axis

--- src/components/charts/barChart/barChart.tsx
+++ src/components/charts/barChart/barChart.tsx
@@ -1,6 +1,7 @@
 import {diagramHeight, diagramWidth, russia} from "../../../Constants";
 import {Bar, Cell, Tooltip, XAxis, YAxis, BarChart as ReBarChart, ResponsiveContainer} from "recharts";
 import styles from "./styles/barChart.module.css";
+import {number} from "prop-types";
 
 export interface BarChartProps<D extends { [key: string | number]: string | number }> {
     data: D[];
@@ -13,14 +14,32 @@ export interface BarChartProps<D extends { [key: string | number]: string | numb
 
 export function BarChart<D extends { [a: string | number]: string | number }>(
     {data, xKey, yKey, color, max}: BarChartProps<D>) {
+    const formatter = (value: number) => {
+        if (value < 1000) {
+            return value.toString();
+        }
+        if (value < 1_000_000) {
+            return Math.ceil(value / 1000).toString() + 'k'
+        }
+        if (value < 1_000_000_000) {
+            return Math.ceil(value / 1_000_000).toString() + 'kk'
+        }
+        return value.toString();
+    }
     return (
         <ResponsiveContainer width="100%" height={400}>
             <ReBarChart className='LineChart' width={window.innerWidth / diagramWidth()}
                         height={window.innerHeight / diagramHeight()}
                         data={data}>
-                <XAxis dataKey={(d) => d[xKey]}/>
+                <XAxis
+                    dataKey={(d) => d[xKey]}
+                    angle={-45}
+                    interval={0}
+                    textAnchor="end"
+                    height={80}
+                />
                 <YAxis
-                    tickFormatter={(value) => new Intl.NumberFormat('en').format(value)}
+                    tickFormatter={(value) => formatter(value)}
                     width={80}
                     domain={[0, max]}
                 />
@@ -30,7 +49,7 @@ export function BarChart<D extends { [a: string | number]: string | number }>(
                         (payload && active && payload.length) &&
                         <div className={styles.tooltip}>
                             <p>{label}</p>
-                            <span>{payload[0].value}</span>
+                            <span>{new Intl.NumberFormat('en').format(Number(payload[0].value))}</span>
                         </div>
                     }
                     formatter={(value) => new Intl.NumberFormat('en').format(Number(value))}
--- src/pages/cases/cases.tsx
+++ src/pages/cases/cases.tsx
@@ -1,26 +1,34 @@
-import {badColor, diagramData} from "../../Constants";
+import {badColor, countriesRu, diagramData} from "../../Constants";
 import {getMainData} from "../../shared/utils/getters";
 
 import {BarChartContainer} from "../../components/charts/barChart/barChartContainer";
 import {useGlobalContext} from "../../shared/context";
+import {WorldActionTypes} from "../../shared/store";
 
 const CasesPage = () => {
     const {worldStates} = useGlobalContext();
-    console.log(worldStates, 1341)
-    if (!worldStates) return <>333</>
+    const worldData = worldStates[WorldActionTypes.allCountriesData]
+    if (!worldStates || !worldData || !worldStates[WorldActionTypes.countriesIds]) return <></>
 
-    if (!worldStates?.allCountriesData) {
-        return <>123</>
-    }
-    const {data, maxValue} =
-        getMainData(worldStates.allCountriesData, "cases", diagramData.cases.label);
+    let maxValue = 0;
+    const data = worldStates[WorldActionTypes.countriesIds]
+        .filter(region => countriesRu.includes(region.name) && region.code !== "10000")
+        .map((region) => {
+            const cases = worldData[Number(region.code)].info.cases;
+            maxValue = Math.max(cases, maxValue);
+            return {
+                x: worldData[Number(region.code)].info.name,
+                y: cases
+            }
+        })
+
     return (
         <BarChartContainer
             key={1}
             color={badColor}
             title='Всего заболеваний'
-            xKey="name"
-            yKey={diagramData.cases.label}
+            xKey="x"
+            yKey="y"
             max={maxValue}
             data={data}/>
     )
